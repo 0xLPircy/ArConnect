969fefc4f5c9a4c1009d4e53d3dd8a725cfc5be2
0xLPircy <taveesha.agarwal@gmail.com>
2024-09-28 22:15:55 +0530

    untested inefficient

--- src/api/modules/user_tokens/user_tokens.background.ts
+++ src/api/modules/user_tokens/user_tokens.background.ts
@@ -10,12 +10,15 @@ import { AO_NATIVE_TOKEN } from "~utils/ao_import";
 
 const background: ModuleFunction<TokenInfoWithBalance[] | TokenInfo[]> = async (
   _,
-  options?: { fetchBalance?: boolean }
+  options?: {
+    fetchBalance?: boolean;
+    tokenIds?: String[];
+  }
 ) => {
   const address = await ExtensionStorage.get("active_address");
   const tokens = (await ExtensionStorage.get<TokenInfo[]>("ao_tokens")) || [];
 
-  if (!options?.fetchBalance) {
+  if (!options?.fetchBalance && !options?.tokenIds) {
     return tokens;
   }
 
@@ -44,7 +47,39 @@ const background: ModuleFunction<TokenInfoWithBalance[] | TokenInfo[]> = async (
     })
   );
 
-  return enrichedTokens;
+  if (!options?.tokenIds) {
+    return enrichedTokens;
+  }
+
+  const selectTokens: TokenInfoWithBalance[] = await Promise.all(
+    tokens.map(async (token) => {
+      let balance: string | null = null;
+
+      try {
+        if (token.processId === AO_NATIVE_TOKEN) {
+          balance = await getNativeTokenBalance(address);
+        } else {
+          const balanceResult = await getAoTokenBalance(
+            address,
+            token.processId
+          );
+          balance = balanceResult.toString();
+        }
+      } catch (error) {
+        console.error(
+          `Error fetching balance for token ${token.Name} (${token.processId}):`,
+          error
+        );
+      }
+      const tidExists = (tokenId) => {
+        return token.processId == tokenId;
+      };
+      if (options.tokenIds.filter(tidExists)) {
+        return { ...token, balance };
+      }
+    })
+  );
+  return selectTokens;
 };
 
 export default background;
